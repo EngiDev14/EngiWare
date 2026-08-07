6b2eac9ae87a215fcc3f520237b9481acf38d413
tanishgarg14011-art <tanishgarg14011@gmail.com>
2026-08-07 11:11:04 +0530

    ..

--- js/notifications.js
+++ js/notifications.js
@@ -11,49 +11,51 @@ updateDoc
 
 const vapidKey = "BM5PUXs3siXz_ruTCAq2vMEUqj1z9Gq3tJv9LZM60ZGo1zlSD6cgL9GyehuLovOnOyljSL9sGNfmDyZRqvrVnSw";
 
-async function registerNotifications(){
+console.log("Notifications JS Loaded 🚀");
 
-if(!("serviceWorker" in navigator))
-return;
+async function registerNotifications() {
+
+try {
+
+if (!("serviceWorker" in navigator)) {
+    console.log("Service Worker not supported");
+    return;
+}
 
 await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+console.log("Service Worker Registered");
 
-const permission =
-await Notification.requestPermission();
+const permission = await Notification.requestPermission();
 
-if(permission !== "granted"){
-console.log("Permission denied");
-return;
+console.log("Permission:", permission);
+
+if (permission !== "granted") {
+    console.log("Permission denied");
+    return;
 }
 
-const token =
-await getToken(messaging,{
-vapidKey
+const token = await getToken(messaging, {
+    vapidKey
 });
 
-console.log(token);
+console.log("FCM Token:", token);
 
-if(auth.currentUser){
+if (auth.currentUser) {
 
-await updateDoc(
-doc(db,"users",auth.currentUser.uid),
-{
-fcmToken: token
-}
-);
+    await updateDoc(
+        doc(db, "users", auth.currentUser.uid),
+        {
+            fcmToken: token
+        }
+    );
 
-console.log("Token Saved");
-
-}
+    console.log("Token Saved Successfully");
 
 }
 
-auth.onAuthStateChanged(async(user)=>{
-
-if(user){
-
-await registerNotifications();
-
+}
+catch(err){
+    console.error("FCM ERROR:", err);
 }
 
-});
+}
